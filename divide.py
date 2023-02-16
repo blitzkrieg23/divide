df1226a7d9a1ec88fb2548c3a3876a973438433f
xzqt23 <angelo.agualada@gmail.com>
2023-02-16 12:05:43 +0800

    Create divide.py

---
+++ divide.py
@@ -0,0 +1,6 @@
+x = float(input("Enter first number: "))
+y = float(input("Enter second number: "))
+
+# z = round(x / y,2) # Round a number to only two decimals
+z = (x / y)
+print(f"{z:.2f}") # format output with 2 decimal value
